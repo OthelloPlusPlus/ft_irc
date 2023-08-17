c2909f9f5054771e32b80965e70939044cdc44dc
Rsshadow2 <94993847+Rsshadow2@users.noreply.github.com>
2023-08-17 15:04:00 +0200

    [FIX] merge issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ohengelm <ohengelm@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/16 15:46:37 by ohengelm      #+#    #+#                 */
+/*   Updated: 2023/08/17 15:03:37 by ohengelm      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Server.hpp"
+#include "colors.hpp"
+
+#include <iostream>
+// std::
+
 int	main(int argc, char **argv)
 {
+	try
+	{
+		Server	server(argc, argv);
+
+		while (true)
+		{
+			server.checkNewClient();
+			server.checkClients();
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr	<< C_RED	<< "Error: "	<< C_RESET
+					<< e.what() << strerror(errno)
+					<< std::endl;
+		return (errno);
+	}
 	return (0);
 }
