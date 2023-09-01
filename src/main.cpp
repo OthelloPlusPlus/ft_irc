49318b12cd33f1fc72adc510b00f7700e543a446
Rsshadow2 <94993847+Rsshadow2@users.noreply.github.com>
2023-09-01 17:48:34 +0200

    [CLEAN] main.cpp was messy

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ohengelm <ohengelm@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/16 15:46:37 by ohengelm      #+#    #+#                 */
-/*   Updated: 2023/09/01 17:34:02 by ohengelm      ########   odam.nl         */
+/*   Updated: 2023/09/01 17:47:41 by ohengelm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 // std::
+
 bool	setEnv(void);
 
 int	main(int argc, char **argv)
@@ -40,37 +41,3 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
-
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// #include <cstdlib> // for setenv
-
-// void	setEnv(void)
-// {
-// 	std::ifstream env(".env");
-// 	if (!env.is_open())
-// 	{
-// 		std::cerr	<< "Failed to open .env"	<< std::endl;
-// 		return ;
-// 	}
-// 	std::string	line;
-// 	while (std::getline(env, line))
-// 	{
-// 		size_t	pos;
-
-// 		pos = line.find("=");
-// 		if (pos = std::string::npos)
-// 			std::cerr	
-// 		std::string	variable = line.substr(line.find_first_not_of(" \t"), line.find_last_not_of(" \t", line.find("=")));;
-// 		std::string	value = line.substr(line.find_first_not_of(" \t", line.find("=") + 1));
-// 		std::cout	<< variable	<< '\n'
-// 					<< value	<< std::endl;
-// 		// size_t	pos = line.find('=');
-// 		// std::cout	<< "setting line: "	<< line	<< std::endl;
-// 		// if (pos != std::string::npos)
-// 		// 	if (setenv(line.substr(0, pos).c_str(), line.substr(pos + 1).c_str(), 1))
-// 		// 		std::cerr	<< "Failed to set env:"	<< line	<< std::endl;;
-// 	}
-// 	env.close();
-// }
