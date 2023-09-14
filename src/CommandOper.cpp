8a70f3011cd4d199dfa94cea26bb5a36cb1d278e
Emanuela <emlicame@student.codam.nl>
2023-09-14 16:20:16 +0200

    [EDIT] removed risk of dangling pointer in for loop

--- src/CommandOper.cpp
+++ src/CommandOper.cpp
@@ -6,7 +6,7 @@
 /*   By: emlicame <emlicame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 12:26:31 by emlicame          #+#    #+#             */
-/*   Updated: 2023/09/13 15:36:11 by emlicame         ###   ########.fr       */
+/*   Updated: 2023/09/14 16:19:07 by emlicame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,29 @@
 
 void	Command::oper(Client &user, const std::string &cmd, const std::vector<std::string> &args, Server *server){
 	if (args.size() != 2){
-		user.sendMsg("461 client " + user.getIpHostName() + " " + cmd + ERR_NEEDMOREPARAMS);
+		user.sendMsg("461 client " + user.getBestName() + " " + cmd + ERR_NEEDMOREPARAMS);
 		return ;
 	}
 		
 	if (server->validatePassword(args[1]) != 2){
-		user.sendMsg("464 client " + user.getIpHostName() + " " + cmd + ERR_PASSWDMISMATCH);
+		user.sendMsg("464 client " + user.getBestName() + " " + cmd + ERR_PASSWDMISMATCH);
 		return ;
 	}
 	if (user.getIsOperator() == false){
-		user.sendMsg(user.getNickName() + " :Admin privileges required");
+		user.sendMsg(user.getBestName() + " :Admin privileges required");
 		return ;
 	}
 	
-	std::vector<Client *>::const_iterator it = server->getClientList().begin();
-	for (; it != server->getClientList().end(); ++it) {
-		Client *u = *it;
-		std::cout << C_YELLOW << u->getNickName() << std::endl;
-		if (u->getNickName() == args[0] && user.getIsOperator() == true){
-			u->setIsOperator(true);
-			user.sendMsg(u->getNickName() + " " + RPL_YOUREOPERM); // ??
+	for (std::vector<Client *>::const_iterator it = server->getClientList().begin(); it != server->getClientList().end(); ++it) {
+		std::cout << C_MGNT << (*it)->getNickName() << std::endl;
+		if ((*it)->getNickName() == args[0] && user.getIsOperator() == true){
+			(*it)->setIsOperator(true);
+			user.sendMsg((*it)->getNickName() + " " + RPL_YOUREOPERM);
 			break;
 		}
-		std::cout << C_RED << u->getNickName() << std::endl;
+		std::cout << C_RED << (*it)->getNickName() << std::endl;
 		if (it == server->getClientList().end()){
-				user.sendMsg(u->getNickName() + " " + ERR_NOOPERHOSTM);
+				user.sendMsg((*it)->getNickName() + " " + ERR_NOOPERHOSTM);
 				return ;
 		}
 	}
@@ -58,40 +56,4 @@ KILL message     Command: KILL   Parameters: <nickname> <comment>
 REHASH message   Command: REHASH Parameters: None
 RESTART message  Command: RESTART Parameters: None
 SQUIT message    Command: SQUIT  Parameters: <server> <comment>
-*/
-
-
-
-/*
-OPER message
-Command: OPER
-Parameters: <name> <password>
-
-The OPER command is used by a normal user to obtain IRC operator privileges. 
-Both parameters are required for the command to be successful.
-
-If the client does not send the correct password for the given name, 
-the server replies with an ERR_PASSWDMISMATCH message and the request is not successful.
-
-If the client is not connecting from a valid host for the given name, 
-the server replies with an ERR_NOOPERHOST message and the request is not successful.
-
-If the supplied name and password are both correct, and the user is connecting 
-from a valid host, the RPL_YOUREOPER message is sent to the user. 
-The user will also receive a MODE message indicating their 
-new user modes, and other messages may be sent.
-
-The <name> specified by this command is separate to the accounts specified 
-by SASL authentication, and is generally stored in the IRCd configuration.
-
-Numeric Replies:
-
-ERR_NEEDMOREPARAMS (461)
-ERR_PASSWDMISMATCH (464)
-ERR_NOOPERHOST (491)
-RPL_YOUREOPER (381)
-Command Example:
-
-  OPER foo bar                ; Attempt to register as an operator
-                              using a name of "foo" and the password "bar".
 */
