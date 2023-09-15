9e5b3ccbbe88f654182479f85f5fd857bad8bf25
Orlando Hengelmolen <ohengelm@f1r4s4.codam.nl>
2023-09-15 18:29:03 +0200

    [EDIT] cleanup

--- src/Command.cpp
+++ src/Command.cpp
@@ -25,7 +25,7 @@ void Command::cleanMsg(Client &user){
 std::vector<std::string> Command::ircSplitMulti(const std::string& input, const std::string& delimiters) {
 	std::vector<std::string> tokens;
 	size_t start = 0, end = 0;
-		
+
 	while ((end = input.find_first_of(delimiters, start)) != std::string::npos) {
 		if (end != start)
 			tokens.push_back(input.substr(start, end - start));
@@ -40,7 +40,7 @@ std::vector<std::string> Command::ircSplit( const std::string& input,
 								const std::string& delimiter){
 	std::vector<std::string> tokens;
 	size_t start = 0, end = 0;
-	
+
 	while ((end = input.find(delimiter, start)) != std::string::npos) {
 		if (end != start)
 			tokens.push_back(input.substr(start, end - start));
@@ -79,11 +79,10 @@ void Command::parseCmd(Client &user, const std::string& cmd, const std::vector<s
 		server->partChannel(&user, args[0]);
 	if (!user.getIsOperator())
 		return;
-		
+
 	if (cmd == "OPER")
 		Command::oper(user, cmd, args, server);
 }
-
 
 void Command::parseMsg(Client &user, Server *server){
 	std::string cmd;
