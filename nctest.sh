71298bce8a2cae17c255616914128335e80ba433
Rsshadow2 <94993847+Rsshadow2@users.noreply.github.com>
2023-10-19 17:15:18 +0200

    [EDIT] made it recognise apple

--- nctest.sh
+++ nctest.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
-IRC_SERVER=$(hostname -I | awk '{print $1}')
+if [ "$(uname)" == "Darwin" ]; then
+	IRC_SERVER=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}')
+else
+	IRC_SERVER=$(hostname -I | awk '{print $1}')
+fi
 PORT=6667
 PASSWORD="password"
 USER1="OthelloNC"
 USER2="OtheNetCat"
 NICK="OthNC"
-
 {
+# exit 0
 	sleep 0.1
-	echo > /dev/tty
+	echo > /dev/tty;
 	echo "PASS $PASSWORD";
 	echo "USER $USER1 * $IRC_SERVER :$USER2";
 	echo "NICK $NICK";
@@ -18,7 +22,7 @@ NICK="OthNC"
 
 # Endof harcoded tests
 	sleep 0.1;
-	echo > /dev/tty
+	echo > /dev/tty;
 	while [ true ]; do
 		read -p "nc (or quit): " COMMAND;
 		if [ "$COMMAND" == "quit" ]; then
@@ -30,4 +34,4 @@ NICK="OthNC"
 		sleep 0.1;
 		echo > /dev/tty
 	done
-} | tee /dev/tty | nc -v $IRC_SERVER $PORT
+} | tee /dev/tty | nc $IRC_SERVER $PORT
